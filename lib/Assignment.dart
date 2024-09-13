e72dc8ae63214b8a130139390c0145cb83f169bb
WijdanShk <wijdan2002khan@gmail.com>
2024-09-13 19:53:56 +0500

    Update Assignment.dart

--- lib/Assignment.dart
+++ lib/Assignment.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Exploration!',
+      title: 'College!',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
